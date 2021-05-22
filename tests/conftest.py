3149244e8ebcb6a5358aef203d861274c6102e54
Bob Belderbos <bobbelderbos@gmail.com>
2021-05-22 12:19:40 +0200

    moved fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,43 @@
+from pathlib import Path
+
 import pytest
+
+import pybites_faker
+from pybites_faker import (CACHE_FILENAME,
+                           PyBitesProvider)
 
 
 def pytest_addoption(parser):
     parser.addoption("--cache", action="store")
+
+
+@pytest.fixture(scope="session")
+def user_cache(pytestconfig):
+    return pytestconfig.getoption("cache")
+
+
+@pytest.fixture(scope="session")
+def cache(tmp_path_factory):
+    path = tmp_path_factory.mktemp(
+        "pybites-fake-data-dir") / CACHE_FILENAME
+    return path
+
+
+@pytest.fixture(scope="session")
+def data(user_cache, cache):
+    """Need to cache once to work with tmp dir
+       and so pickle can actually load the data"""
+    if user_cache is not None:
+        cache = Path(user_cache)
+
+    force_reload = not cache.exists()
+    data = pybites_faker.create_pb_data_object(
+        cache=cache,
+        force_reload=force_reload)
+
+    return data
+
+
+@pytest.fixture(scope="session")
+def pb_faker(data):
+    return PyBitesProvider(data)
--- tests/test_pybites_faker.py
+++ tests/test_pybites_faker.py
@@ -1,48 +1,12 @@
-from pathlib import Path
 import re
 
 import pytest
 
-import pybites_faker
-from pybites_faker import (CACHE_FILENAME,
-                           PyBitesProvider,
-                           Bite, Article)
+from pybites_faker import Bite, Article
 from pybites_faker.exceptions import NoDataForCriteria
 
 LEVELS = ["intro", "beginner",
           "intermediate", "advanced"]
-
-
-@pytest.fixture(scope="session")
-def user_cache(pytestconfig):
-    return pytestconfig.getoption("cache")
-
-
-@pytest.fixture(scope="session")
-def cache(tmp_path_factory):
-    path = tmp_path_factory.mktemp(
-        "pybites-fake-data-dir") / CACHE_FILENAME
-    return path
-
-
-@pytest.fixture(scope="session")
-def data(user_cache, cache):
-    """Need to cache once to work with tmp dir
-       and so pickle can actually load the data"""
-    if user_cache is not None:
-        cache = Path(user_cache)
-
-    force_reload = not cache.exists()
-    data = pybites_faker.create_pb_data_object(
-        cache=cache,
-        force_reload=force_reload)
-
-    return data
-
-
-@pytest.fixture(scope="session")
-def pb_faker(data):
-    return PyBitesProvider(data)
 
 
 def test_str_representation(pb_faker):
